386a04381f508253d6a84f390afa7ae236ac9949
lukxnut <kristian2015ok@gmail.com>
2024-04-22 21:45:28 +0700

    Add: Query SQL

---
+++ TestQuery.php
@@ -0,0 +1,20 @@
+<?php
+
+require_once "./GetConnection.php";
+
+$connection = getConnection();
+
+$sql = "Select id, name, email from customers";
+$result = $connection->query($sql);
+
+foreach ($result as $row) {
+    $id = $row["id"];
+    $name = $row["name"];
+    $email = $row["email"];
+
+    echo "Id : $id" . PHP_EOL;
+    echo "name : $name" . PHP_EOL;
+    echo "email : $email" . PHP_EOL;
+}
+
+$connection = null;
